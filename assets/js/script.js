e142bc189a3632eeb5b01f8cb3ac73682c9594ed
Phuong Tran <132531844+txphi@users.noreply.github.com>
2024-04-18 09:16:40 +0700

    feat: add animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,17 @@ var main_content = document.getElementById('main-content');
 // when u click on video => hide video and show main content
 intro_video.addEventListener('click', function () {
     intro_video.style.display = 'none';
-    main_content.style.display = 'block';
+    main_content.style.opacity = 1;
+    document.body.style.height = 'auto';
+    document.body.style.overflowY = 'auto';
 });
+
+// reload, scroll to top
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+    // reset style
+    intro_video.style.display = 'block';
+    main_content.style.opacity = 0;
+    document.body.style.height = '100vh';
+    document.body.style.overflowY = 'hidden';
+};
